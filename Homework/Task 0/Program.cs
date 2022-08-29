a3ec036cd1891b36c3c9179caaff0fa3cffdcc58
Mikhail <mehan@list.ru>
2022-08-29 21:35:41 +0300

    Task is ready

--- Homework/Task 0/Program.cs
+++ Homework/Task 0/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿//Написать программу, которая принимает два числа и определяет, какое из них больше, а какое меньше
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int firstNum = Prompt("Введите первое число: ");
+int secondNum = Prompt("Введите второе число: ");
+
+if (firstNum > secondNum)
+{
+    System.Console.WriteLine($"Число {firstNum} большее, а {secondNum} меньшее.");
+}
+else
+{
+    if (firstNum < secondNum)
+    {
+        System.Console.WriteLine($"Число {secondNum} большее, а {firstNum} меньшее.");
+    }
+    else
+    {
+            System.Console.WriteLine($"Числа равны!");
+
+    }
+}
