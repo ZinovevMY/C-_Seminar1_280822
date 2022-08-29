4ff90f5b2d4e8661d7b4ca94358797d2fa0ca60b
Mikhail <mehan@list.ru>
2022-08-29 20:55:34 +0300

    Task is ready

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//Напишите программу вычисления функции x = f(a)
+//Примем f(a) = (4x - 1)/2
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    double value = double.Parse(strValue);
+    return value;
+}
+System.Console.WriteLine("Вычислим значение f(a) = (4x - 1)/2");
+double number = Prompt("Введите значение x: ");
+double result = (4*number - 1)/2;
+System.Console.WriteLine($"Результат вычисления равен {result}");
