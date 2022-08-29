f4f739a4534d44998c417f0c342ee02dab8c8b53
Mikhail <mehan@list.ru>
2022-08-29 21:23:10 +0300

    Task is ready

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Написать программу, которая на вход принимает значение, а на выходе выдает обратное значение
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    double value = double.Parse(strValue);
+    return value;
+}
+
+double number = Prompt("Введите любое положительное число: ");
+
+if (number > 0)
+{
+    System.Console.WriteLine($"Обратным значение числа {number} является {1/number}");
+}
+else
+{
+    System.Console.WriteLine("Вы ввели неверное число!");
+}
