046d4f88113bf15d03e63ae027106a7dc21491ef
Mikhail <mehan@list.ru>
2022-08-29 20:38:08 +0300

    Task is ready

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+string[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+int dayNumber = Prompt("Введите интересующий номер дня недели: ");
+
+if (dayNumber < 1 || dayNumber > 7)
+{
+    System.Console.WriteLine("Нет такого дня недели!");
+}
+else
+{
+    System.Console.WriteLine(days[dayNumber-1]);
+}
