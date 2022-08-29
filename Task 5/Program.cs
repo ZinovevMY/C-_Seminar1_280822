e5b82ad913799a361e38794e27d2e58037b32241
Mikhail <mehan@list.ru>
2022-08-29 21:12:52 +0300

    Task is ready

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Напишите программу вычисления модуля числа
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int number = Prompt("Введите любое целое число: ");
+
+if (number < 0)
+{
+    System.Console.WriteLine($"Модуль числа {number} равен {-number}");
+}
+else
+{
+    System.Console.WriteLine($"Модуль числа {number} равен {number}");
+}
