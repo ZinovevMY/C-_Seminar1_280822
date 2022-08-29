de92e24f3270fe7d759a1c740259b1223ade17f3
Mikhail <mehan@list.ru>
2022-08-29 21:05:46 +0300

    Task is ready

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//Напишите программу, которая на вход принимает число N, а на выходе выдает все целые числа в промежутке от -N до N
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int number = Prompt("Введите любое положительное целое число: ");
+
+System.Console.WriteLine($"Все целые числа в интервале от -{number} до {number}: ");
+for (int i = -number; i <= number; i++)
+{
+    System.Console.Write(i + " ");
+}
