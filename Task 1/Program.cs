cdf91f0678b8f18b46e88a599459cf1e63f279d4
Mikhail <mehan@list.ru>
2022-08-29 20:30:55 +0300

    Task is ready

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int firstNum = Prompt("Введите первое число: ");
+int secondNum = Prompt("Введите второе число: ");
+int sqrFirstNum = firstNum * firstNum;
+
+if (secondNum > firstNum)
+{
+    if (secondNum == sqrFirstNum)
+    {
+        System.Console.WriteLine($"Число {secondNum} является квадратом числа {firstNum}");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {secondNum} не является квадратом числа {firstNum}");
+    }
+}
+else
+{
+    System.Console.WriteLine("Первое введенное число больше второго!");
+}
