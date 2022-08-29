907b1e63f0a64c8e074809b2afb605ac43b16fa2
Mikhail <mehan@list.ru>
2022-08-29 20:19:17 +0300

    Task is ready

--- Task 0/Program.cs
+++ Task 0/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int number = Prompt("Введите любое целое число: ");
+int sqrNumber = number * number;
+System.Console.WriteLine($"Квадрат числа {number} равен {sqrNumber}");
