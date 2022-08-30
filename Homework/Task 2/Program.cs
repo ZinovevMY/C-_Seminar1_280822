7b587146db352e43159d3b657452714a2a52dcd4
Mikhail <mehan@list.ru>
2022-08-30 20:44:51 +0300

    Task is done

--- Homework/Task 2/Program.cs
+++ Homework/Task 2/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿//Написать программу, принимающую на вход число и определяющую, является ли оно четным
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int number = Prompt("Введите любое целое число: ");
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"Число {number} четное.");
+}
+else
+{
+    System.Console.WriteLine($"Число {number} нечетное.");
+}
+
