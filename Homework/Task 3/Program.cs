3a110385ac8ad31763c01a1a36c83dc8359ca287
Mikhail <mehan@list.ru>
2022-08-30 20:51:09 +0300

    Task is done

--- Homework/Task 3/Program.cs
+++ Homework/Task 3/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿//Написать программу, принимающую число N и выводящую все четные числа от 1 до N
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int number = Prompt("Веедите любое положительное целое число: ");
+
+for (int i = 2; i <= number; i = i + 2)
+{
+    System.Console.Write(i + " ");
+}
